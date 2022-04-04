624c4cdd32239c5e0ba9b9b47511d8342c4167ff
Rebecca Jackson <rbca.jackson@gmail.com>
2022-04-04 11:07:13 -0700

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+with open(here + "/README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="hiccupy",
     py_modules=["hiccupy"],
-    version="0.0.1",
-    description="",
+    version="1.0.0",
+    description="Rendering HTML from hiccup-style lists in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Rebecca Jackson",
     author_email="rbca.jackson@gmail.com",
     classifiers=[
